56cbb6a29c485a32d2f43fa73e6ec6aedd82a676
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-10-20 18:10:45 +0000

    Saving broken code to improve upon.

--- lib/cli.rb
+++ lib/cli.rb
@@ -17,16 +17,27 @@ class CLI
     user_input = gets.strip.downcase
 
     #if the user says Hayao Miyazaki
-      if user_input == "Hayao Miyazaki" || "Hayao"
-        puts "The man, the myth, the legend! Let's take a look at his movies."
+      if user_input = "Hayao Miyazaki" || "Hayao"
+        puts "The man, the myth, the legend! Let's take a look at his films."
       #now we need to iterate through all the movies, to find the director, and return his movies.
         display_Hayao_movies
+      elsif user_input = "Gorō Miyazaki" || "Goro Miyazaki"
+        puts "Let's take a look at Goro's films!"
+        display_Goro_movies
       end
     end
 
     def display_Hayao_movies
       Movie.all.each do |movie|
         if movie.director == "Hayao Miyazaki"
+          puts movie.title
+        end
+      end
+    end
+
+    def display_Goro_movies
+      Movie.all.each do |movie|
+        if movie.director == "Gorō Miyazaki"
           puts movie.title
         end
       end
