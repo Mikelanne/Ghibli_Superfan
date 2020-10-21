0d9b79b9c08cd1c1a3b4156fa95ff79b2a16e3e2
Mikelanne Wood <mikelanne.wood@gmail.com>
2020-10-21 17:07:39 +0000

    Basic completion of the app.

--- lib/cli.rb
+++ lib/cli.rb
@@ -14,6 +14,10 @@ class CLI
     if user_input == "yes"
       display_movie_list
       ask_user_for_movie_choice
+
+      menu
+    else
+      puts "Goodbye! Enjoy your Ghibli adventure!"
     end
   end
 
@@ -40,33 +44,7 @@ class CLI
     puts "Great choice! #{movie.title} is one of the best!"
     puts "#{movie.title} was released in #{movie.release_date}."
     puts "The director is #{movie.director}."
+    puts "#{movie.title}'s synopsis is: #{movie.description}."
+    puts "#{movie.title} recieved a score of #{movie.rt_score} on Rotten Tomatoes. Not so bad!"
   end
-    #if the user says Hayao Miyazaki
-    #   if user_input == "Hayao Miyazaki" || "Hayao"
-    #     puts "The man, the myth, the legend! Let's take a look at his films."
-    #   #now we need to iterate through all the movies, to find the director, and return his movies.
-    #     display_Hayao_movies
-    #   end
-    #   if user_input = "Gorō Miyazaki" || "Goro Miyazaki"
-    #     puts "Let's take a look at Goro's films!"
-    #     display_Goro_movies
-    #   end
-    # end
-    #
-    # def display_Hayao_movies
-    #   Movie.all.each do |movie|
-    #     if movie.director == "Hayao Miyazaki"
-    #       puts movie.title
-    #     end
-    #   end
-    # end
-    #
-    # def display_Goro_movies
-    #   Movie.all.each do |movie|
-    #     if movie.director == "Gorō Miyazaki"
-    #       puts movie.title
-    #     end
-    #   end
-    # end
-
 end
